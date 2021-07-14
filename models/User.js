aab3f449e7fa8d0aa9689723d6a2a8003c4efa35
Max <linme00p@gmail.com>
2021-07-14 17:16:33 +0300

    Create User model

---
+++ models/User.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 255,
+    },
+    email: {
+        type: String,
+        required: true,
+        max: 255,
+    },
+    password: {
+        type: String,
+        required: true,
+        max: 1024,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+export default mongoose.model('User', userSchema);
