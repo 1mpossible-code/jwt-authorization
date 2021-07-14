cff5f69b9854f0ea0cac6304467b43e599e43efe
Max <linme00p@gmail.com>
2021-07-14 17:35:50 +0300

    Add basic register implementation in routes/auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,22 @@
 import express from 'express';
+import User from "../models/User.js";
 
 const router = express.Router();
 
-router.post('/register', (req, res) => {
-    res.send('Register');
+router.post('/register', async (req, res) => {
+    console.log(req.body)
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    });
+
+    try {
+        const savedUser = await user.save();
+        res.send(savedUser);
+    } catch (err) {
+        res.status(400).send(err);
+    }
 })
 
 export default router;
