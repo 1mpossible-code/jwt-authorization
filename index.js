b078d5fcd83ed81ce786bee26335f60c1aa936f8
Max <linme00p@gmail.com>
2021-07-14 17:08:14 +0300

    Connect to MongoDB via 'mongoose' with 'dotenv'.

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 import express from 'express';
 import authRouter from "./routes/auth.js";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+// Create express app
 const app = express();
 
+// Connect to DB
+mongoose.connect(process.env.DB_CONNECT, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log('Database connected'))
+    .catch((err) => console.error(err));
+
+// Auth routes middleware
 app.use('/api/user', authRouter);
 
+// Serve server
 app.listen(3000, () => {
     console.log('Server is running http://localhost:3000/')
 })
