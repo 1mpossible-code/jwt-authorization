0aa3bc149f7b565ac74667a5252de91ce1c66d8d
Max <linme00p@gmail.com>
2021-07-15 17:38:33 +0300

    Move auth login logic from controller to service

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,22 +1,12 @@
-import User from "../models/User.js";
-import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken';
-import {validateLogin} from "../validation.js";
+import {createJwtToken, validateLoginData} from "../services/authSerivce.js";
 
 export const store = async (req, res) => {
     // Validate the data before login
-    const {error} = validateLogin(req.body);
+    const {message, user} = await validateLoginData(req.body);
     // Send error is has one
-    if (error) return res.status(400).send(error.details[0].message);
-
-    // Check if user exists
-    const user = await User.findOne({email: req.body.email});
-    if (!user) return res.status(400).send('Email not found');
-    // Check if the password is valid
-    const passwordIsValid = await bcrypt.compare(req.body.password, user.password);
-    if (!passwordIsValid) return res.status(400).send('Password is invalid');
+    if (message) return res.status(400).send(message);
 
     // Create and assign jwt token
-    const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
+    const token = await createJwtToken({_id: user._id});
     res.header('Authorization', `Bearer ${token}`).send(token);
 }
--- services/authSerivce.js
+++ services/authSerivce.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js";
 import bcrypt from 'bcryptjs';
-import {validateRegistration} from "../validation.js";
+import jwt from 'jsonwebtoken';
+import {validateLogin, validateRegistration} from "../validation.js";
 
 export const validateRegisterData = async (data) => {
     // Validate the data before make a user
@@ -11,6 +12,23 @@ export const validateRegisterData = async (data) => {
     // Check if email is unique
     const emailExists = await User.findOne({email: data.email});
     if (emailExists) return {message: 'Email already exists'};
+}
+
+export const validateLoginData = async (data) => {
+    // Validate the data before make a user
+    const {error} = validateLogin(data);
+    // Send error is has one
+    if (error) return {message: error.details[0].message};
+
+    // Check if user exists
+    const user = await User.findOne({email: data.email});
+    if (!user) return {message: 'Email not found'};
+
+    // Check if the password is valid
+    const passwordIsValid = await bcrypt.compare(data.password, user.password);
+    if (!passwordIsValid) return {message: 'Password is invalid'};
+    // Return user
+    return {user};
 }
 
 export const hashPassword = async (password) => {
@@ -31,4 +49,9 @@ export const createNewUser = async ({name, email, password}) => {
     } catch (error) {
         return {status: 400, payload: error};
     }
+}
+
+export const createJwtToken = async (payload) => {
+    // Create jwt token
+    return jwt.sign(payload, process.env.TOKEN_SECRET);
 }
