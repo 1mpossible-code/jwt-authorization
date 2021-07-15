7a8c6ba88784639c8d5bb6cfc6ec6cb9afe3026d
Max <linme00p@gmail.com>
2021-07-15 16:46:34 +0300

    Use auth middleware instead the verifying token

---
+++ middlewares/auth.js
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken';
+import User from "../models/User.js";
+
+export default async (req, res, next) => {
+    const token = req.header('Authorization').replace('Bearer ', '');
+    if (!token) return res.status(401).send('Access Denied');
+
+    try {
+        const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+        req.user = await User.findOne({_id: verified._id});
+        next()
+    } catch (e) {
+        res.status(400).send('Error:', e);
+    }
+};
