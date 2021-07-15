5b02fe60ae5f4a3903ca870a0190d1446556f3e9
Max <linme00p@gmail.com>
2021-07-15 16:47:16 +0300

    Update testAuth routes with auth middleware

--- routes/testAuth.js
+++ routes/testAuth.js
@@ -1,15 +1,18 @@
 import express from 'express';
-import User from "../models/User.js";
+import authMiddleware from "../middlewares/auth.js";
 
 const router = express.Router();
 
-router.get('/', ((req, res) => {
-    // Find user by _id
-    const user = User.findOne({_id: req.user._id});
-    // If user not exists send an error
-    if (!user) return res.status(400).send('Invalid user id');
+router.get('/', authMiddleware, async (req, res) => {
+    // Get user
+    const user = req.user;
     // Send user
-    res.json(user);
-}))
+    res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        date: user.date,
+    });
+});
 
 export default router;
