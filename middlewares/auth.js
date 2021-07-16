5d243e3b0d0ff83d105c86287a608cea7001f275
Max <linme00p@gmail.com>
2021-07-16 16:45:57 +0300

    Update middlewares/auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 import User from "../models/User.js";
 
 export default async (req, res, next) => {
-    const authorization = req.header('Authorization');
+    const authorization = req.header('Authorization') || req.cookies.jwt;
     if (!authorization) return res.status(401).send('Access Denied');
     const token = authorization.replace('Bearer ', '');
 
@@ -10,7 +10,7 @@ export default async (req, res, next) => {
         const verified = jwt.verify(token, process.env.TOKEN_SECRET);
         req.user = await User.findOne({_id: verified._id});
         next();
-    } catch (e) {
-        res.status(400).send('Error:', e);
+    } catch {
+        res.status(400).send('Token is invalid');
     }
 };
