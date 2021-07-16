37faeaa14b3f19f9dcb8b5b8c3ca5468338e0e9d
Max <linme00p@gmail.com>
2021-07-16 16:09:13 +0300

    Update middlewares/auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,8 +2,9 @@ import jwt from 'jsonwebtoken';
 import User from "../models/User.js";
 
 export default async (req, res, next) => {
-    const token = req.header('Authorization').replace('Bearer ', '');
-    if (!token) return res.status(401).send('Access Denied');
+    const authorization = req.header('Authorization');
+    if (!authorization) return res.status(401).send('Access Denied');
+    const token = authorization.replace('Bearer ', '');
 
     try {
         const verified = jwt.verify(token, process.env.TOKEN_SECRET);
